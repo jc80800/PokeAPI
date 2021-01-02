03e35e722b993d5a7f9076204517081543433239
jc8080 <jc8080@rit.edu>
2021-01-02 14:54:28 -0500

    Implemented MaterialCSS to create "cards" to fit each pokemon. Implemented async/await to fetch information consistently

--- script.js
+++ script.js
@@ -1,30 +1,72 @@
-function fetchPokemon(){
-    for(let i = 1; i < 151; i++){
-        searchPokemon(i);
+async function fetchPokemon(){
+    for(let i = 1; i < 151  ; i++){
+        await searchPokemon(i);
     }
 }
 
-function createCard(data){
-    const container = document.querySelector('#pokemonContainer');
-    const element = document.createElement('div');
-    const name = document.createElement('h1');
-    const id = document.createElement('p');
-    const img = document.createElement('img');
-    img.src = `${data.sprites.front_default}`
-    element.appendChild(img);
-    name.innerHTML = data.name;
-    id.innerHTML = data.id;
-    element.appendChild(name);
-    element.appendChild(id);
+function createCard(data, id){
+    const col = document.createElement('div');
+    col.className = "col s4";
+
+    const card = document.createElement('div');
+    card.className = "card";
+
+    const cardImage = document.createElement('img');
+    cardImage.src = data.sprites.front_default;
     
-    container.appendChild(element)
+    const cardTitle = document.createElement('span');
+    cardTitle.className = "card-title";
+
+    const cardTitleColor = document.createElement('span');
+    cardTitleColor.className = "cardTitleColor";
+    cardTitleColor.innerHTML = data.name;
+
+    const cardContent = document.createElement('div');
+    cardContent.className = "card-content"
+
+    const pokeId = document.createElement('h2');
+    pokeId.innerHTML = id;
+
+    cardContent.appendChild(pokeId);
+
+    data.types.forEach(element => {
+        const type = element.type.name;
+        const info = document.createElement('p');
+        info.innerHTML = type;
+        cardContent.appendChild(info);
+    })
+
+    col.appendChild(card);
+    card.appendChild(cardImage);
+    card.appendChild(cardTitle)
+    cardTitle.appendChild(cardTitleColor);
+    card.appendChild(cardContent);
+
+    document.querySelector('#pokemonContainer').lastElementChild.appendChild(col);
 }
+
+function createDiv(data, id){
+    const container = document.querySelector('#pokemonContainer');
+
+    if ((id % 3) === 1){
+        const row = document.createElement('div');
+        row.className = "row";
+        container.appendChild(row);
+        createCard(data, id);
+    }
+
+    else{
+        createCard(data, id);
+    }
+
+}
+
 
 function searchPokemon(id) {
     const BASE_URL = `https://pokeapi.co/api/v2/pokemon/${id}`;
     fetch(BASE_URL)
         .then(res => res.json())
-        .then(data => createCard(data));
+        .then(data => createDiv(data, id));
 }
 
 fetchPokemon()
